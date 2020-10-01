78a493dc1bcd6fc9d616079b851c08d0eb11d4b9
siyulyu <lyu.si@husky.neu.edu>
2020-09-30 21:33:36 -0700

    modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,23 @@ class Header extends React.Component {
     render() {
        return(
      
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">
-                <span class="d-block d-lg-none">Siyu Lyu</span>
-                {/* <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="" /></span>
-                <span class="d-none d-lg-block"><img src="profile1.png"  /></span> */}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                <span className="d-block d-lg-none">Siyu Lyu</span>
+                {/* <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile.jpg" alt="" /></span>
+                <span className="d-none d-lg-block"><img src="profile1.png"  /></span> */}
                 
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
                 </ul>
             </div>
         </nav>
